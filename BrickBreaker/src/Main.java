2dc3d078024b1da81c1e9c4ec9c91247039b4fd8
ersanebit <ersan.ebit@gmail.com>
2018-02-12 18:38:32 +0100

    Second game in java

---
+++ BrickBreaker/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Ersan on 9/4/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+       JFrame obj = new JFrame();
+       Gameplay gamePlay = new Gameplay();
+       obj.setBounds(10,10,700,600);
+       obj.setTitle("Breakout Ball");
+       obj.setResizable(false);
+       obj.setVisible(true);
+       obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       obj.add(gamePlay);
+    }
+}
